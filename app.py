a382888c928370200352969e19baa49d7f817f6e
Sameni Narayan Rao <53487866+SameniNarayanRao@users.noreply.github.com>
2021-05-03 11:07:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open("fp_knn.pkl", "rb"))
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+@app.route('/predict', methods = ['POST'])
+def home():
+    AG = request.form['AG']
+    HLAP = request.form['HLAP']
+    COF = request.form['COF']
+    array = np.array([[AG, HLAP, COF]])      
+    pred = model.predict(array)
+    return render_template("result.html", df = pred)
+
+if __name__ == "__main__":
+    app.run(debug = True)
